1e0eef2f32bbf5ffc10a4323bd9faf2c83542a35
prabhakaran <hellospk7@gmail.com>
2015-08-03 08:47:23 +0530

    linux command changes

--- src/com/linux/cmd/ls.go
+++ src/com/linux/cmd/ls.go
@@ -1,1 +1,17 @@
 package cmd
+
+//list command
+/**
+Possibilities:
+	-a : do not ignore entries starting with .
+
+*/
+import (
+	"fmt"
+	"os"
+)
+
+func Execute() {
+	fmt.Println("Execute ls command")
+	parse(os.Args[1:], LS_SUPPORT_OPTS)
+}
